fa92562018cb0f55c022b67f4a25ce7ecaeb6906
devotech <57639218+devotech@users.noreply.github.com>
2021-12-16 12:05:21 +0100

    Update Call-Replace.ps1
    removed "-debug" from the call script

--- Call-Replace.ps1
+++ Call-Replace.ps1
@@ -7,5 +7,5 @@ $fileStartsWith = 'log4j-core'
 
 Start-Transcript -Path ".\Logs\$Server-replace.log"
 Import-Module .\unClass-jar\ReplaceJarFileWithPatchFile.ps1
-ReplaceJarFileWithPatchFile -Folder $folder -FileExtension $fileExtension -PatchFileExtension $patchFileExtension -BackupFileExtension $backupFileExtension -FileStartsWith $fileStartsWith -Verbose -Recurse -Debug
+ReplaceJarFileWithPatchFile -Folder $folder -FileExtension $fileExtension -PatchFileExtension $patchFileExtension -BackupFileExtension $backupFileExtension -FileStartsWith $fileStartsWith -Verbose -Recurse
 Stop-Transcript
